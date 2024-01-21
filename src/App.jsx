d8eb924aac2d8c2466ad4090210ff865ec5e8fa7
stanbishal <bishalbhattarai0124@gmail.com>
2024-01-21 14:17:25 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500 text-center">Hello world!</h1> 
+      <h1 className="text-center">Bishal</h1>
+    </div>
+  )
+}
+
+export default App
